e61ff1d66cd87870dfda7cead67b6801be960ef4
akashr725 <akashr725@gmail.com>
2024-04-03 19:05:18 +0530

    Akash:Hibernate1st

---
+++ src/mypack/Empbeans.java
@@ -0,0 +1,26 @@
+package mypack;
+
+public class Empbeans {
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	private String Name,Address;
+
+}
